f45c61a6791ae01390eb5fc74c113cb46499609d
MahlanguRonny <MahlanguRonny@gamil.com>
2014-03-25 00:19:33 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+R4STechs::Application.config.secret_key_base = '2368a34c823853749b76f6b7340fd33e8a5412d926d07b6b3130385b0ab6d1172f7e8b779c7dd54d62dde84abc03b7f9b747574219cd60e0820ab617489eb377'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+R4STechs::Application.config.session_store :cookie_store, key: '_R4STechs_session'
