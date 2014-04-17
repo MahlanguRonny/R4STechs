d154f1a5565de6ab4203f166ab649992cf9b3948
MahlanguRonny <MahlanguRonny@gamil.com>
2014-04-17 14:32:09 +0200

    Still saving changes for product CRUD by admin

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -13,5 +13,5 @@ ActiveAdmin.register Product do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+  permit_params :name, :price, :quantity, :image_url
 end
---
+++ app/views/contacts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @contact, :email, :name, :query, :created_at, :updated_at
