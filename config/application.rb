adf22e6e91e18e5d91395fb9ef6beac5a8c20225
MahlanguRonny <MahlanguRonny@gamil.com>
2014-04-17 15:07:42 +0200

    makin changes for heroku active admin migration

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module R4STechs
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w[active_admin.css active_admin.js print.css]
   end
 end
