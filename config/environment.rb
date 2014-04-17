e8973680eabb1b95b3e29959a6e1c9055aacc823
MahlanguRonny <MahlanguRonny@gamil.com>
2014-04-17 16:57:08 +0200

    pushing

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 R4STechs::Application.initialize!
+config.action_mailer.default_url_options = { :host => 'whispering-headland-7216.heroku.com' }
