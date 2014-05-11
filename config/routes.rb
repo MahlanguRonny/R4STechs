2dc8addd00a71d9b4e324246b62784356b759067
MahlanguRonny <MahlanguRonny@gamil.com>
2014-05-11 17:48:38 +0200

    pushing changes on the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,79 +1,5 @@
-<<<<<<< HEAD
-R4STechs::Application.routes.draw do
-  resources :line_items
+#<<<<<<< HEAD
 
-  resources :carts
-  get "catalog/MyCatalog"
-  get "store/catalog"
-
-  resources :store_products
-
-  #devise_for :admin_users, ActiveAdmin::Devise.config
-  #ActiveAdmin.routes(self)
-  #devise_for :users
-  root  'static_pages#home'
-  match '/about', to: 'static_pages#about', via: 'get'
-  match '/contactus', to: 'static_pages#contactus', via: 'get'
-  match '/account', to: 'static_pages#account', via: 'get'
-  match '/shoppingCart', to: 'static_pages#shoppingCart', via: 'get'
-  match '/login', to: 'static_pages#login', via: 'get'
-  match '/catalog', to: 'catalog#MyCatalog', via: 'get' 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-end
-=======
 R4STechs::Application.routes.draw do
   resources :my_items
 
@@ -91,8 +17,8 @@ R4STechs::Application.routes.draw do
 
   resources :store_products
 
-  ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  #ActiveAdmin::Devise.config
+  #ActiveAdmin.routes(self)
   devise_for :users
   root  'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
@@ -159,4 +85,4 @@ R4STechs::Application.routes.draw do
   #     resources :products
   #   end
 end
->>>>>>> starting-products-afresh
+#>>>>>>> starting-products-afresh
